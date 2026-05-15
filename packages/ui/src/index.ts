ecb1d5534c5afca6f830bbc5bae5d9a2bd0e1a01
bntvllnt <bntvllnt@users.noreply.github.com>
2026-05-15 14:17:43 +0200

    fix(ui): export registry snippet helper hooks

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -19,6 +19,15 @@ export {
   type SupportedLanguage as UISupportedLanguage,
 } from "./lib/types";
 export { useDebounce } from "./lib/use-debounce";
+export {
+  useDocumentEventListener,
+  useEventCallback,
+  useWindowEventListener,
+} from "./lib/use-event-callback";
 export { useHorizontalScroll } from "./lib/use-horizontal-scroll";
 export { useMounted } from "./lib/use-mounted";
+export {
+  useControllableState,
+  useUncontrolledState,
+} from "./lib/use-uncontrolled-state";
 export { cn } from "./lib/utils";
